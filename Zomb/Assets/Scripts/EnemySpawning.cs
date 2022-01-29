a6d53dae8016b50626a57428b713b6494e06c000
AnikaThatavarthy <anikathatavarthy@vt.edu>
2022-01-29 15:35:03 -0500

    Bare-bones enemy spawning

---
+++ Zomb/Assets/Scripts/EnemySpawning.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawning : MonoBehaviour
+{
+    public GameObject[] spawnPoints;
+    GameObject currentPoint;
+    int index;
+    
+    public GameObject[] enemies;
+    public float minTimeBtwSpawns;
+    public float maxTimeBtwSpawns;
+    public bool canSpawn;
+    public float spawnTime;
+    public int enemiesInRoom;
+
+    public bool spawnerDone;
+    public GameObject spawnerDoneGameObject;
+
+    pribate void Start(){
+        Invoke("SpawnEnemy", 0.5f);
+    }
+
+    void SpawnEnemy(){
+        index = Random.Range(0, spawnPoints.Length);
+        currentPoint = SpawnPoints[index];
+        float timeBtwSpans = Random.Range(minTimeBtwSpawns, maxTimeBtwSpawns);
+
+        if(canSpawn){
+            Instantiate(enemies[Random.Range(0, enemies.length)], currentPoint.transform.position, Quaternion.identity);
+            enemiesInRoom++;
+        }
+
+        Invoke("SpawnEnemy", timeBtwSpans);
+
+        if(spawnerDone){
+            //up to us
+            spawnerDoneGameObject.SetActive(true);
+        }
+    }
+}
